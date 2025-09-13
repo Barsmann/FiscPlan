6dcf3cd41ed3d19465ab316ec33a6399f4eed567
Barsmann <mrneilharris@gmail.com>
2025-09-13 21:16:02 +0100

    test(portal): add /login alias

---
+++ apps/portal/src/app/login/page.tsx
@@ -0,0 +1,6 @@
+﻿export default function Login() {
+  return <main className='min-h-screen bg-black text-white p-10'>
+    <h1 className='text-3xl font-semibold'>Login</h1>
+    <p className='mt-2 text-neutral-400'>Alias route for /signin.</p>
+  </main>;
+}
